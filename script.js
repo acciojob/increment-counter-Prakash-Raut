a04533d6b9429d98abed3b23e8e39ffaeddc81f8
Prakash Raut <prakash_raut@outlook.com>
2025-07-30 10:13:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+const count = document.getElementById("counter");
+const incBtn = document.getElementById("incrementBtn");
+
+incBtn.addEventListener("click", () => {
+	let currVal = parseInt(count.innerText);
+	alert(currVal);
+	count.innerText = currVal + 1;
+});
